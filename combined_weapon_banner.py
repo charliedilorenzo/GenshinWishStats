da40ae8c8316b0bdaa88016eb881a1299604cf58
Charlie DiLorenzo <charlie.dilorenzo64@gmail.com>
2022-08-01 19:15:21 -0400

    added nearly full function

--- combined_weapon_banner.py
+++ combined_weapon_banner.py
@@ -1,3 +1,4 @@
+from re import I
 from numpy import full
 import WishSim
 import WishStats
@@ -21,8 +22,31 @@ def main(user_data, action_list):
 
     return 0
 
-def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character_pity = 0, character_guaranteed = 0, weapon_pity = 0, weapon_guaranteed = 0, epitomized_path = 0, silenced = True ):
-    #
+def character_then_weapon_stats(total_pulls = 0, character_copies = 7, weapon_copies = 1, character_pity = 0, character_guaranteed = 0, weapon_pity = 0, weapon_guaranteed = 0, epitomized_path = 0, silenced = True, trials = 10000):
+    character_successes = {True: 0, False:0}
+    weapon_sucesses = {True:0,False:0}
+    character_rateup_count = { i : 0 for i in range(0,character_copies+1) }
+    weapon_rateup_count = { i : 0 for i in range(0,weapon_copies+1) }
+    pulls_remaining = {}
+    for i in range(0,trials):
+        output = character_then_weapon_sim(total_pulls = total_pulls, character_copies = character_copies, weapon_copies = weapon_copies, character_pity = character_pity, character_guaranteed = character_guaranteed, 
+                                    weapon_pity = weapon_pity, weapon_guaranteed = weapon_guaranteed, epitomized_path = epitomized_path, silenced = silenced)
+        #output -> {"character_goal_achieved": character_goal_achieved, "weapon_goal_achieved": weapon_goal_achieved, "character_rateup_count": character_wish_sim.rateup_count,
+        #                                         "weapon_rateup_count": weapon_wish_sim.rateup_count, "wishes_remaining": total_pulls}
+        character_successes[output["character_goal_achieved"]]+=1
+        weapon_sucesses[output["weapon_goal_achieved"]]+=1
+        character_rateup_count[output["character_rateup_count"]]+=1
+        if ("weapon_rateup_count" in output.keys()):
+            weapon_rateup_count[output["weapon_rateup_count"]]+=1
+        if output["wishes_remaining"] in pulls_remaining.keys():
+            pulls_remaining[output["wishes_remaining"]]+=1
+        else:
+            pulls_remaining[output["wishes_remaining"]]=1
+    helpers.combined_weapon_to_breakdown(character_successes,weapon_sucesses, character_rateup_count, weapon_rateup_count,trials)
+    return character_successes,weapon_sucesses, character_rateup_count, weapon_rateup_count
+
+def character_then_weapon_sim(total_pulls = 0, character_copies = 7, weapon_copies = 1, character_pity = 0, character_guaranteed = 0, weapon_pity = 0, weapon_guaranteed = 0, epitomized_path = 0, silenced = True ):
+    # this is good for putting on return I think
     character_goal_achieved = False
     weapon_goal_achieved = False
 
@@ -31,17 +55,21 @@ def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character
     banner_end_date  = userinput.BANNER_END_DATE
 
     # initialize values
-    num_primos = userinput.NUM_PRIMOS
-    num_fates = userinput.NUM_FATES
-    num_starglitter = userinput.NUM_STARGLITTER
-    num_genesis = userinput.NUM_GENESIS
+    if total_pulls == 0:
+        num_primos = userinput.NUM_PRIMOS
+        num_fates = userinput.NUM_FATES
+        num_starglitter = userinput.NUM_STARGLITTER
+        num_genesis = userinput.NUM_GENESIS
+        total_pulls = math.floor((num_primos+num_genesis)/160)+num_fates + math.floor(num_starglitter/5)
+    else:
+        total_pulls = total_pulls
     current_pity = character_pity
     current_guaranteed = character_guaranteed
-    total_pulls = math.floor((num_primos+num_genesis)/160)+num_fates + math.floor(num_starglitter/5)
-
-    print()
-    helpers.print_messaged_banner("CHARACTER BANNER", mode="triple_line")
-    print()
+    
+    if not silenced:
+        print()
+        helpers.print_messaged_banner("CHARACTER BANNER", mode="triple_line")
+        print()
     # --------------------------- CHARACTER BANNER PART ---------------------------
     ru_five_stars = ["Baizhu"]
     ru_four_stars = ["Partner for Baizhu"]
@@ -50,9 +78,6 @@ def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character
     five_stars = consts.STANDARD_FIVE_STAR_CHARACTERS
 
     banner_type = "character"
-    user_data = {}
-    user_data.update( {"num_primos": num_primos, "num_fates": num_fates, "num_genesis": num_genesis, "num_starglitter": num_starglitter, "desired_five_star": desired_five_star,
-    "current_pity": current_pity, "current_guaranteed": current_guaranteed, "desired_ru": desired_ru, "banner_end_date": banner_end_date, "banner_type": banner_type, "ru_five_stars": ru_five_stars})
     character_wish_sim = WishSim.WishSim(ru_four_stars, four_stars, ru_five_stars, five_stars, desired_five_star = desired_five_star,  banner_type = banner_type)
     character_wish_sim.roll(total_pulls, 0, desired_ru, silenced = silenced, set_pity = current_pity,set_guaranteed=current_guaranteed)
     # we calculate how many pulls we have left from the other one
@@ -60,10 +85,10 @@ def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character
     character_pity = character_wish_sim.pity
     wishes_used = character_wish_sim.number_pulled
     total_pulls = total_pulls-wishes_used
-
-    print()
-    helpers.print_messaged_banner("WEAPON BANNER", mode="triple_line")
-    print()
+    if not silenced:
+        print()
+        helpers.print_messaged_banner("WEAPON BANNER", mode="triple_line")
+        print()
 
     if character_wish_sim.rateup_count >= desired_ru:
         character_goal_achieved = True
@@ -79,8 +104,6 @@ def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character
         desired_ru =  weapon_copies
         five_stars = consts.STANDARD_FIVE_STAR_WEAPONS
         banner_type = "weapon"
-        user_data.update( {"num_primos": num_primos, "num_fates": num_fates, "num_genesis": num_genesis, "num_starglitter": num_starglitter, "desired_five_star": desired_five_star,
-        "current_pity": current_pity, "current_guaranteed": current_guaranteed, "desired_ru": desired_ru, "banner_end_date": banner_end_date, "banner_type": banner_type, "ru_five_stars": ru_five_stars})
         weapon_wish_sim = WishSim.WishSim(ru_four_stars, four_stars, ru_five_stars, five_stars, desired_five_star = desired_five_star,  banner_type = banner_type)
         weapon_wish_sim.roll(total_pulls, 0, desired_ru, silenced = silenced, set_pity = current_pity,set_guaranteed=current_guaranteed)
         weapon_guaranteed = weapon_wish_sim.guaranteed
@@ -88,11 +111,11 @@ def character_then_weapon_sim(character_copies = 7, weapon_copies = 1, character
         wishes_used = weapon_wish_sim.number_pulled
         total_pulls = total_pulls - wishes_used
         if weapon_wish_sim.rateup_count >= desired_ru:
-            character_goal_achieved = True
+            weapon_goal_achieved = True
         return_package.update({"weapon_rateup_count": weapon_wish_sim.rateup_count,})
 
-    return_package = {character_goal_achieved: character_goal_achieved, weapon_goal_achieved: weapon_goal_achieved, "character_rateup_count": character_wish_sim.rateup_count,
-                         "wishes_remaining": total_pulls }
+    return_package.update({"character_goal_achieved": character_goal_achieved, "weapon_goal_achieved": weapon_goal_achieved, "character_rateup_count": character_wish_sim.rateup_count,
+                         "wishes_remaining": total_pulls })
     return return_package
 
 
